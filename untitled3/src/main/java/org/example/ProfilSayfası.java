58f2a7bf7707a70d26febb73ea2d629b1011f299
cebrail <cemg346807@gmail.com>
2025-02-06 18:58:34 +0300

    yeni eklentiler var

--- untitled3/src/main/java/org/example/ProfilSayfası.java
+++ untitled3/src/main/java/org/example/ProfilSayfası.java
@@ -1,15 +1,22 @@
-﻿package org.example;
+package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ProfilSayfası {
     JFrame frame = new JFrame();
+    JLabel label = new JLabel();
 
     ProfilSayfası(){
+
+        label = new JLabel("isim cebrail");
+
+
         frame.setTitle("prfil sayısı");
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        frame.setLayout(new GridLayout());
+        frame.add(label);
         frame.setVisible(true);
     }
 
