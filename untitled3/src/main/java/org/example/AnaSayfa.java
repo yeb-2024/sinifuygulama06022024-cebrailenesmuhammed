d2f6502749948fbdd7fde059f133a8f25ee12e7b
cebrail <cemg346807@gmail.com>
2025-02-06 18:39:03 +0300

    button eklendi

--- untitled3/src/main/java/org/example/AnaSayfa.java
+++ untitled3/src/main/java/org/example/AnaSayfa.java
@@ -4,10 +4,18 @@ import javax.swing.*;
 
 public class AnaSayfa {
     JFrame frame = new JFrame();
+    JButton button = new JButton();
 
     AnaSayfa(){
+
+        button = new JButton("buton");
+        button.setBounds(200,200,100,50);
+        button.setFocusable(false);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
         frame.setVisible(true);
     }
 
