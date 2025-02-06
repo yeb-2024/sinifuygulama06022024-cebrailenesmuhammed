1830c92ea59c22edd532a6ccd6981d267b03e2bd
cebrail <cemg346807@gmail.com>
2025-02-06 18:32:34 +0300

    ana sayfa sınıf olustruldu

---
+++ untitled3/src/main/java/org/example/AnaSayfa.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class AnaSayfa {
+    JFrame frame = new JFrame();
+
+    AnaSayfa(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
